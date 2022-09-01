720b61eb6338349201ccd3abc8da1851594d3985
amangrewal1 <50376555+amangrewal1@users.noreply.github.com>
2022-09-01 16:37:28 -0700

    Time: 50 ms (80.92%), Space: 14.3 MB (55.59%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,19 +1,32 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left,right = 0,len(nums)-1
-        while left<=right:
-            mid = (left+right)//2
-            if nums[mid]==target:
-                return mid
-            elif nums[left]>nums[mid]:
-                if nums[mid]<target<=nums[right]:
-                    left = mid+1
-                else:
-                    right = mid-1
-            else:
-                if nums[left]<=target<nums[mid]:
-                    right = mid-1
-                else:
-                    left = mid+1
-                    
+        
+        maxi = nums.index(max(nums))
+        
+        if (nums[0] <= target and target <= nums[maxi]) :
+            l = 0
+            r = maxi
+            while l <= r :
+                mid = (l+r) // 2
+                if target < nums[mid] :
+                    r = mid - 1
+                elif target > nums[mid] :
+                    l = mid + 1
+                elif target == nums[mid] :
+                    return mid
+                else :
+                    return -1
+        elif maxi < (len(nums) - 1) :
+            l = maxi + 1
+            r = len(nums) - 1
+            while l <= r :
+                mid = (l+r) // 2
+                if target < nums[mid] :
+                    r = mid - 1
+                elif target > nums[mid] :
+                    l = mid + 1
+                elif target == nums[mid] :
+                    return mid
+                else :
+                    return -1  
         return -1
