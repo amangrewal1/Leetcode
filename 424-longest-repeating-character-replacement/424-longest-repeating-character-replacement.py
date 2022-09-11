3dfbd671ded6d724e519dd6e576f277534832175
amangrewal1 <50376555+amangrewal1@users.noreply.github.com>
2022-09-10 18:40:54 -0700

    Time: 843 ms (5.00%), Space: 14 MB (57.22%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -0,0 +1,25 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+        count = {}
+        l = 0 
+        r = 0
+        winLen = r - l + 1
+        res = 0
+        
+        while l <= r and r < len(s):
+            if s[r] not in count:
+                count[s[r]] = 1
+            else :
+                count[s[r]] += 1
+            
+            if (r-l +1) - max(count.values())  <= k :
+                res = max(res, r-l+1)
+                r += 1
+            else :
+                res = max(res, r-l)
+                count[s[l]] -= 1
+                count[s[r]] -= 1
+                l += 1
+            
+        return res
