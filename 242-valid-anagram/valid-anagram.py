4c0705aac54543841a3ef884d4a2cad13cd67f21
amangrewal1 <50376555+amangrewal1@users.noreply.github.com>
2025-03-13 21:04:46 -0700

    Time: 15 ms (43.24%) | Memory: 18.5 MB (19.76%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,19 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        d1 = {}
-        d2 = {}
-
-        for letter in s:
-            if letter not in d1:
-                d1[letter] = 0
-            d1[letter] += 1
-        
-        for letter in t:
-            if letter not in d2:
-                d2[letter] = 0
-            d2[letter] += 1
-
-        if d1 == d2:
-            return True
-        else :
-            return False
+        return sorted(s) == sorted(t)
