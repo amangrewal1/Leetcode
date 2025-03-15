a6de5f1c59415057a64fda6f7d422a8e632dfb48
amangrewal1 <50376555+amangrewal1@users.noreply.github.com>
2025-03-14 19:47:56 -0700

    Time: 15 ms (10.58%) | Memory: 24 MB (24.88%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,7 +2,7 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
         d1 = {}
-        d2 = {}
+        freq = [[] for i in range(len(nums) + 1)]
         res = []
 
         for i in nums:
@@ -11,14 +11,13 @@ class Solution:
             d1[i] += 1
         
         for key, value in d1.items():
-            if value not in d2:
-                d2[value] = []
-            d2[value].append(key)
+            freq[value].append(key)
         
-        for i in reversed(sorted(d2.keys())):
-            for i in d2[i]:
-                res.append(i)
+        for i in range(len(freq) -1, 0, -1):
+            for j in freq[i]:
+                res.append(j)
                 k -= 1
             if k == 0:
                 return res
+
 
