ddf4ba75c81ac1e251582248db72872792460664
amangrewal1 <50376555+amangrewal1@users.noreply.github.com>
2025-03-15 18:16:30 -0700

    Time: 6 ms (84.65%) | Memory: 18 MB (70.12%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        s = s.lower()
+        s = s.replace(" ","")
+        
+        p1 = 0
+        p2 = len(s) - 1
+
+        if s == "":
+            return True
+    
+
+        while (p1 <= p2):
+            while (s[p1].isalnum() != True and p1 < p2):
+                p1 += 1
+            while (s[p2].isalnum() != True and p1 < p2):
+                p2 -= 1
+            if s[p1] == s[p2]:
+                p1 += 1
+                p2 -= 1
+            else: 
+                return False
+        
+        return True
