ca34322830801f6156cffd571223626bb99fcda9
amangrewal1 <50376555+amangrewal1@users.noreply.github.com>
2025-03-21 01:42:05 -0700

    Time: 155 ms (26.14%) | Memory: 18.2 MB (77.70%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -13,6 +13,7 @@ class Solution:
             window[c] = 0
         have = 0
         need = len(t)
+        reslen = 0
         
         for i in range(len(s)):
             if s[i] in t:
@@ -21,10 +22,12 @@ class Solution:
                     have += 1
             while have == need:
                 sub = s[l:i+1]
-                if res == "":
+                if reslen == 0:
                     res = sub
-                elif len(res) > len(sub):
-                    res = sub 
+                    reslen = i - l +1
+                elif reslen > (i - l +1):
+                    res = sub
+                    reslen = i - l +1 
                 if s[l] in t:
                     window[s[l]] -= 1
                     if window[s[l]] < tmap[s[l]]:
