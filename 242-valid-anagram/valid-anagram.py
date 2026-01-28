360739f7f9c46ed10dcd14e4ab3bf867c4a06e40
amangrewal1 <50376555+amangrewal1@users.noreply.github.com>
2026-01-27 20:15:17 -0800

    Time: 5 ms (93.57%) | Memory: 19.6 MB (17.88%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,19 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        d1 = {}
+        from collections import Counter
+        cnt_s = Counter(s)
+        cnt_t = Counter(t)
 
-        for letter in s:
-            if letter not in d1:
-                d1[letter] = 0
-            d1[letter] += 1
-        
-        for letter in t:
-            if letter not in d1:
-                return False
-            d1[letter] -= 1
-
-        for val in d1.values():
-            if val != 0:
-                return False
-        
-        return True
+        return cnt_s == cnt_t
